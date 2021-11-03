d5370678a8f1af4c991a77317c66653fccea2be8
CharlieGao2009 <93461099+CharlieGao2009@users.noreply.github.com>
2021-11-03 10:07:57 +0800

    Create FunnyVirus 0_1_2.cpp

---
+++ FunnyVirus 0_1_2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<Windows.h>
+using namespace std;
+int main()
+{
+	system("color 0a");
+	printf("%s","惊喜，你的电脑中病毒了\n\n\n大 危\n\n\n");
+	Sleep(2500);
+	system("tree");
+	int n=20;
+	while (n--) system("dir");
+	printf("正在销毁以上文件……请稍等\n");
+	printf("%s%d%s%d%s%d%s%d%s%d","%",20,"\n%",40,"\n%",60,"\n%",80,"\n%",100);
+	Sleep(6500);
+	printf("%s","\n\n\n\n\n\n\n\n\n    						愚  人  节  快  乐 [/doge]\n\n\n\n\n\n");
+	Sleep(4000);
+	printf("%s","\nCopyright Giao子 2020~2021  FunnyVirus 0.1.2");
+	printf("%s","\n\n    						按Q+Enter键退出……\n");
+	char q;
+	cin>>q;
+	if (q=='q') return 0;
+	return 0;
+}
+
+/*  Copyright Giao子 2020~2021
+	FunnyVirus 0.1.2
+	Time :"2021/09/06 20:33 UTC+8:00
+*/
