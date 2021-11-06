67a82dec63304ea78626f400af368decaebb2a3d
CharlieGao2009 <93461099+CharlieGao2009@users.noreply.github.com>
2021-11-06 14:10:25 +0800

    Create FunnyVirus 0_4_1_Pre-release_1.cpp

---
+++ FunnyVirus 0_4_1_Pre-release_1.cpp
@@ -0,0 +1,352 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+void color()
+{
+	system("color 01");Sleep(50);
+	system("color 02");Sleep(50);
+	system("color 03");Sleep(50);
+	system("color 04");Sleep(50);
+	system("color 05");Sleep(50);
+	system("color 06");Sleep(50);
+	system("color 07");Sleep(50);
+	system("color 08");Sleep(50);
+	system("color 09");Sleep(50);
+	system("color 0a");Sleep(50);
+	system("color 0b");Sleep(50);
+	system("color 0c");Sleep(50);
+	system("color 0d");Sleep(50);
+	system("color 0e");Sleep(50);
+	system("color 0f");Sleep(50);
+	system("color 10");Sleep(50);
+	system("color 12");Sleep(50);
+	system("color 13");Sleep(50);
+	system("color 14");Sleep(50);
+	system("color 15");Sleep(50);
+	system("color 16");Sleep(50);
+	system("color 17");Sleep(50);
+	system("color 18");Sleep(50);
+	system("color 19");Sleep(50);
+	system("color 1a");Sleep(50);
+	system("color 1b");Sleep(50);
+	system("color 1c");Sleep(50);
+	system("color 1d");Sleep(50);
+	system("color 1e");Sleep(50);
+	system("color 1f");Sleep(50);
+	system("color 20");Sleep(50);
+	system("color 21");Sleep(50);
+	system("color 23");Sleep(50);
+	system("color 24");Sleep(50);
+	system("color 25");Sleep(50);
+	system("color 26");Sleep(50);
+	system("color 27");Sleep(50);
+	system("color 28");Sleep(50);
+	system("color 29");Sleep(50);
+	system("color 2a");Sleep(50);
+	system("color 2b");Sleep(50);
+	system("color 2c");Sleep(50);
+	system("color 2d");Sleep(50);
+	system("color 2e");Sleep(50);
+	system("color 2f");Sleep(50);
+	system("color 30");Sleep(50);
+	system("color 31");Sleep(50);
+	system("color 32");Sleep(50);
+	system("color 34");Sleep(50);
+	system("color 35");Sleep(50);
+	system("color 36");Sleep(50);
+	system("color 37");Sleep(50);
+	system("color 38");Sleep(50);
+	system("color 39");Sleep(50);
+	system("color 3a");Sleep(50);
+	system("color 3b");Sleep(50);
+	system("color 3c");Sleep(50);
+	system("color 3d");Sleep(50);
+	system("color 3e");Sleep(50);
+	system("color 3f");Sleep(50);
+	system("color 40");Sleep(50);
+	system("color 41");Sleep(50);
+	system("color 42");Sleep(50);
+	system("color 43");Sleep(50);
+	system("color 45");Sleep(50);
+	system("color 46");Sleep(50);
+	system("color 47");Sleep(50);
+	system("color 48");Sleep(50);
+	system("color 49");Sleep(50);
+	system("color 4a");Sleep(50);
+	system("color 4b");Sleep(50);
+	system("color 4c");Sleep(50);
+	system("color 4d");Sleep(50);
+	system("color 4e");Sleep(50);
+	system("color 4f");Sleep(50);
+	system("color 50");Sleep(50);
+	system("color 51");Sleep(50);
+	system("color 52");Sleep(50);
+	system("color 53");Sleep(50);
+	system("color 54");Sleep(50);
+	system("color 56");Sleep(50);
+	system("color 57");Sleep(50);
+	system("color 58");Sleep(50);
+	system("color 59");Sleep(50);
+	system("color 5a");Sleep(50);
+	system("color 5b");Sleep(50);
+	system("color 5c");Sleep(50);
+	system("color 5d");Sleep(50);
+	system("color 5e");Sleep(50);
+	system("color 5f");Sleep(50);
+	system("color 60");Sleep(50);
+	system("color 61");Sleep(50);
+	system("color 62");Sleep(50);
+	system("color 63");Sleep(50);
+	system("color 64");Sleep(50);
+	system("color 65");Sleep(50);
+	system("color 67");Sleep(50);
+	system("color 68");Sleep(50);
+	system("color 69");Sleep(50);
+	system("color 6a");Sleep(50);
+	system("color 6b");Sleep(50);
+	system("color 6c");Sleep(50);
+	system("color 6d");Sleep(50);
+	system("color 6e");Sleep(50);
+	system("color 6f");Sleep(50);
+	system("color 70");Sleep(50);
+	system("color 71");Sleep(50);
+	system("color 72");Sleep(50);
+	system("color 73");Sleep(50);
+	system("color 74");Sleep(50);
+	system("color 75");Sleep(50);
+	system("color 76");Sleep(50);
+	system("color 78");Sleep(50);
+	system("color 79");Sleep(50);
+	system("color 7a");Sleep(50);
+	system("color 7b");Sleep(50);
+	system("color 7c");Sleep(50);
+	system("color 7d");Sleep(50);
+	system("color 7e");Sleep(50);
+	system("color 7f");Sleep(50);
+	system("color 80");Sleep(50);
+	system("color 81");Sleep(50);
+	system("color 82");Sleep(50);
+	system("color 83");Sleep(50);
+	system("color 84");Sleep(50);
+	system("color 85");Sleep(50);
+	system("color 86");Sleep(50);
+	system("color 87");Sleep(50);
+	system("color 89");Sleep(50);
+	system("color 8a");Sleep(50);
+	system("color 8b");Sleep(50);
+	system("color 8c");Sleep(50);
+	system("color 8d");Sleep(50);
+	system("color 8e");Sleep(50);
+	system("color 8f");Sleep(50);
+	system("color 90");Sleep(50);
+	system("color 91");Sleep(50);
+	system("color 92");Sleep(50);
+	system("color 93");Sleep(50);
+	system("color 94");Sleep(50);
+	system("color 95");Sleep(50);
+	system("color 96");Sleep(50);
+	system("color 97");Sleep(50);
+	system("color 98");Sleep(50);
+	system("color 9a");Sleep(50);
+	system("color 9b");Sleep(50);
+	system("color 9c");Sleep(50);
+	system("color 9d");Sleep(50);
+	system("color 9e");Sleep(50);
+	system("color 9f");Sleep(50);
+	system("color a0");Sleep(50);
+	system("color a1");Sleep(50);
+	system("color a2");Sleep(50);
+	system("color a3");Sleep(50);
+	system("color a4");Sleep(50);
+	system("color a5");Sleep(50);
+	system("color a6");Sleep(50);
+	system("color a7");Sleep(50);
+	system("color a8");Sleep(50);
+	system("color a9");Sleep(50);
+	system("color ab");Sleep(50);
+	system("color ac");Sleep(50);
+	system("color ad");Sleep(50);
+	system("color ae");Sleep(50);
+	system("color af");Sleep(50);
+	system("color b0");Sleep(50);
+	system("color b1");Sleep(50);
+	system("color b2");Sleep(50);
+	system("color b3");Sleep(50);
+	system("color b4");Sleep(50);
+	system("color b5");Sleep(50);
+	system("color b6");Sleep(50);
+	system("color b7");Sleep(50);
+	system("color b8");Sleep(50);
+	system("color b9");Sleep(50);
+	system("color ba");Sleep(50);
+	system("color bc");Sleep(50);
+	system("color bd");Sleep(50);
+	system("color be");Sleep(50);
+	system("color bf");Sleep(50);
+	system("color c0");Sleep(50);
+	system("color c1");Sleep(50);
+	system("color c2");Sleep(50);
+	system("color c3");Sleep(50);
+	system("color c4");Sleep(50);
+	system("color c5");Sleep(50);
+	system("color c6");Sleep(50);
+	system("color c7");Sleep(50);
+	system("color c8");Sleep(50);
+	system("color c9");Sleep(50);
+	system("color ca");Sleep(50);
+	system("color cb");Sleep(50);
+	system("color cd");Sleep(50);
+	system("color ce");Sleep(50);
+	system("color cf");Sleep(50);
+	system("color d0");Sleep(50);
+	system("color d1");Sleep(50);
+	system("color d2");Sleep(50);
+	system("color d3");Sleep(50);
+	system("color d4");Sleep(50);
+	system("color d5");Sleep(50);
+	system("color d6");Sleep(50);
+	system("color d7");Sleep(50);
+	system("color d8");Sleep(50);
+	system("color d9");Sleep(50);
+	system("color da");Sleep(50);
+	system("color db");Sleep(50);
+	system("color dc");Sleep(50);
+	system("color de");Sleep(50);
+	system("color df");Sleep(50);
+	system("color e0");Sleep(50);
+	system("color e1");Sleep(50);
+	system("color e2");Sleep(50);
+	system("color e3");Sleep(50);
+	system("color e4");Sleep(50);
+	system("color e5");Sleep(50);
+	system("color e6");Sleep(50);
+	system("color e7");Sleep(50);
+	system("color e8");Sleep(50);
+	system("color e9");Sleep(50);
+	system("color ea");Sleep(50);
+	system("color eb");Sleep(50);
+	system("color ec");Sleep(50);
+	system("color ed");Sleep(50);
+	system("color ef");Sleep(50);
+	system("color f0");Sleep(50);
+	system("color f1");Sleep(50);
+	system("color f2");Sleep(50);
+	system("color f3");Sleep(50);
+	system("color f4");Sleep(50);
+	system("color f5");Sleep(50);
+	system("color f6");Sleep(50);
+	system("color f7");Sleep(50);
+	system("color f8");Sleep(50);
+	system("color f9");Sleep(50);
+	system("color fa");Sleep(50);
+	system("color fb");Sleep(50);
+	system("color fc");Sleep(50);
+	system("color fd");Sleep(50);
+	system("color fe");Sleep(50);
+	
+	system("color 0a");
+}
+void title_zh()
+{
+	system("title 惊喜，你的电脑中病毒了");
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t惊喜，你的电脑中病毒了\n\n\n\t\t\t\t\t\t\t大 危\n\n\n";
+	color();
+	Sleep(2500);
+	system("tree c:\\");
+}
+void title_en()
+{
+	system("title Suprise!!!Your computer is hacked!!!!!");
+	cout<<"\n\n\n\n\n\n\n\n\n\t\t\t\t\tSuprise!!!Your computer is hacked!!!!!\n\n\n\t\t\t\t\t\tYou are DANGEROUS now!!\n\n\n";
+	color();
+	Sleep(2500);
+	system("tree c:\\");
+}
+void dir_and_del_zh()
+{
+	int n=20;
+	while (n--) system("dir C:\\Windows\\SysWOW64");
+	cout<<"正在销毁以上文件……请稍等\n%20\n%40\n%60\n%80\n%100";
+	Sleep(6500);
+	system("cls");
+}
+void dir_and_del_en()
+{
+	int n=20;
+	while (n--) system("dir C:\\Windows\\SysWOW64");
+	cout<<"Deleting files...... Please wait.\n%20\n%40\n%60\n%80\n%100";
+	Sleep(6500);
+	system("cls");
+}
+void fool_zh()
+{
+	cout<<"\n\n\n\n\n\n\n\n\n    						愚  人  节  快  乐 [/doge]\n\n\n\n\n\n";
+}
+void fool_en()
+{
+	cout<<"\n\n\n\n\n\n\n\n\n    						Happy Fool's Day!![/doge]\n\n\n\n\n\n";
+}
+void copyright()
+{
+	cout<<"\nCopyright Giao子 2020~2021  FunnyVirus 0.4.1 Pre-release 1\n\n";
+}
+void key_zh()
+{
+	cout<<"    						按Q+Enter键退出……\n";
+}
+void key_en()
+{
+	cout<<"							Press Q+Enter to quit...\n";
+}
+void press()
+{
+	char q;
+	cin>>q;
+	while (1)
+	{
+		if (q=='q') return;
+		else cin>>q;
+	}
+}
+void zh()
+{
+	title_zh();
+	dir_and_del_zh();
+	fool_zh();
+	copyright();
+	key_zh();
+	press();
+}
+void en()
+{
+	title_en();
+	dir_and_del_en();
+	fool_en();
+	copyright();
+	key_en();
+	press();
+}
+void none()
+{
+	cout<<"无此语言！  No such language!";
+	Sleep(1000);
+}
+int main()
+{
+	ios::sync_with_stdio(1);
+	cin.tie(0);
+	cout.tie(0);
+	cout<<"选择一个语言。  Pick a language.\n				ZH=简体中文  EN=English(US)\n";
+	string lang;
+	cin>>lang;
+	system("cls");
+	if (lang=="ZH") zh();
+	else if (lang=="EN") en();
+	else none();
+	return 0;
+}
+
+/*  Copyright Giao子 2020~2021
+	FunnyVirus 0.4.1 Pre-release 1
+	Time :"2021/11/05 14:18 UTC+8:00
+*/
